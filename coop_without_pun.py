3a49d872d0fa9ce8a03342c07aac7aed5c83535e
Ivan Chizurum Ezeigbo <ivyezeigbo@yahoo.com>
2019-05-08 00:27:59 -0700

    Update coop_without_pun.py

--- coop_without_pun.py
+++ coop_without_pun.py
@@ -17,7 +17,7 @@ d_1 = 0 #number of games where players defected more than cooperated
 while stat <= limit:
  
     Network = {} #dictionary of networks
-    total_num = 64
+    total_num = 36
     defect = sample(range(1, total_num + 1), int(total_num/2))
     players = [] #nodes
     for i in range(total_num):
@@ -77,7 +77,7 @@ while stat <= limit:
         while not Connect: #makes sure graph is connected
             G = nx.Graph()   
             G.add_nodes_from(players)
-            path = random_layering() #function that calls for diversity in every round of a game. Commenting this line of code removes diversity in the rounds of the game
+            #path = random_layering() #function that calls for diversity in every round of a game. Commenting this line of code removes diversity in the rounds of the game
             G.add_edges_from(path)
             if nx.is_connected(G): #makes sure graph is a connected graph
                 Connect = True
@@ -92,7 +92,7 @@ while stat <= limit:
         
         for j in range(len(Network)):
             labels[j] = Network[j][1]
-
+        '''
         pos = nx.spring_layout(G)
         colors = []
         for n in players:
@@ -103,7 +103,7 @@ while stat <= limit:
         nx.draw(G, pos, node_color = colors)
         nx.draw_networkx_labels(G, pos,labels, font_size = 14)
         plt.show() #plots the network using Networkx Python Package. Uncommenting these four lines of code makes it possible to visually see and follow how a strategy emerges in the network
-        
+        '''
 
         #payoffs without costly punishment
         '''
@@ -179,7 +179,7 @@ while stat <= limit:
                 Same = False
                 break
         if Same == True:  #case for everyone having the same strategy
-            #plotter()
+            #plotter() #Uncomment this to print graph
             if Network[z][1] == 'D':
                 #print('Everyone is a defector\n')
                 strategy.append('Defect')
@@ -217,7 +217,7 @@ while stat <= limit:
     #print('\nIt took %s games' %count)
 
     stat += 1
-print('Cope:', c_1, ' Defe:', d_1)
+#print('Cope:', c_1, ' Defe:', d_1)
 coding = Counter(strategy)
 if 'Cooperate' not in coding:
     coding['Cooperate'] = 0
